0d02e00d2d7dd10942587cef0e53f17d2288b6ba
Tuấn Anh <tuananhlg97@gmail.com>
2018-12-19 19:29:27 +0700

    front-end-22

---
+++ Lession 08/Bài tập 1/scriptHw1.js
@@ -0,0 +1,40 @@
+for (var i = 7; i >= 1; i--) {
+	for (var j = i; j <= 7; j++) {
+		document.writeln("*");
+	}
+document.writeln("<br/>");
+}
+
+document.writeln("<br/>");
+
+for (var i = 1; i <= 7; i++) {
+	for (var j = 1; j <= 7; j++) {
+		if (j>i) {
+			document.writeln("*");
+		}
+		else {
+		document.writeln(" ");
+			
+		}
+	}
+document.writeln("<br/>");
+
+}
+
+document.writeln("<br/>");
+
+for (var i = 1; i <= 7; i++) {
+	for (var j = i; j <= 7; j++) {
+		document.writeln("*");
+	}
+document.writeln("<br/>");
+}
+
+document.writeln("<br/>");
+
+/*for (var i = 1; i <= 7; i++) {
+	for (var j = i; j <= 7; j++) {
+		document.writeln("*");
+	}
+document.writeln("<br/>");
+}*/
